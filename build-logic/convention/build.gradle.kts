c51d89c6fbbd13d640d5911cd23a1ff6ccb6c2e2
Chris <siuro514@gmail.com>
2025-06-11 09:23:16 +0800

    Add build-logic convention build.gradle.kts

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.example.imagesearch.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
